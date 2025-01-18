3cc1761d7accf51412a35fe10ea1644c37a306fc
Jason Herbert <herbertj289@gmail.com>
2025-01-18 16:43:49 +0100

    Stepping more into further use of conditional logic

---
+++ if_else.py
@@ -0,0 +1,20 @@
+#See logical_operators.py for context
+var1 = 609
+var2 = 67
+
+
+#if-else script to determine equity
+if var1 == var2:
+    print("The variables are the same")
+else:
+    print("The variables are different")
+
+
+#if-else script to determine which is greater
+if var1 > var2:
+    print(f"{var1} is greater than {var2}")
+else:
+    print(f"{var2} is greater than {var1}")
+
+
+
