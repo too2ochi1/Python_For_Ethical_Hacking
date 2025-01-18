9077b1d4b564f3c3403a24981c2edb1591166364
Jason Herbert <herbertj289@gmail.com>
2025-01-18 15:53:29 +0100

    feeling out the area of basic arithimetic operations in the world of Python such as: Addition(+); Subtraction(-); Multiplication(*); Division(/); and Modulus(%).

---
+++ arithimetic_operators.py
@@ -0,0 +1,30 @@
+number1 = 24
+number2 = 3
+
+string1 = "Hello"
+string2 = "Bye"
+
+#Plus
+plus = number1 + number2
+print(plus)
+
+plus_string = string1 + string2
+print(plus_string)
+
+#Subtraction
+sub = number1 - number2
+print(sub)
+
+#Multiplication
+mult = number1*number2
+times = string1*number2
+print(mult*times)
+
+#Division
+div = number1/number2
+print(div)
+
+#Module
+mod = number2%number1
+print(mod)
+
