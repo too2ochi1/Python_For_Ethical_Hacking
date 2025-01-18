7e17d1e98d7c2cfe910fcd2b683a5254a3df25a8
Jason Herbert <herbertj289@gmail.com>
2025-01-18 16:36:54 +0100

    strings, floats, integers, and a little on functions and how the function *pun intended*

--- data_type.py
+++ data_type.py
@@ -14,6 +14,14 @@ VOCABULARY
 "integer" is a number as a variable character, without a decimal
 You can concatenate strings amongst each other, but not with integers and/or floats
 
+In Python, you can print a string and an integer together using several methods. Here is the most prefered one:
+
+Using f-strings (Python 3.6+):
+
+name = "John"
+age = 25
+
+print(f"{name} is {age} years old.")
 """
 print(greeting1 + greeting2)
 print(type(greeting1))
