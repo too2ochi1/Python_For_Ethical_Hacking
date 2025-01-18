533132acdbcaf67970dd3e33df926c4367a83e26
Jason Herbert <herbertj289@gmail.com>
2025-01-18 16:03:02 +0100

    Here we are using simple boolean statements to identify basic logic

---
+++ logical_operators.py
@@ -0,0 +1,21 @@
+#This makes use of basic boolean statements to identify basic logic
+var1 = 5
+var2 = 4
+
+#Equals
+print(var1==var2)
+
+#Different
+print(var1!=var2)
+
+#Greater than/Equal to
+print(var1>var2)
+print(var1>=var2)
+
+#Less than/Equal to
+print(var1<var2)
+print(var1<=var2)
+
+
+
+
