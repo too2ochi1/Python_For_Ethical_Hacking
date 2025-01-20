6580be44b52c0bf897d348f37148ad14466e558a
Jason Herbert <herbertj289@gmail.com>
2025-01-20 22:32:17 +0100

    The sequel to conditional statements, introduced the 'elif' functon in addition to 'if-else'

---
+++ elif.py
@@ -0,0 +1,19 @@
+# 7 >= ==> A
+# 5 >= and 7 < ==> B
+# 3 >= and 5 < ==> C
+# 3 < ==> F
+
+score = 9
+
+if score >= 7:
+    print("A")
+elif 5 <= score < 7:
+    print("B")
+elif 3 <= score < 5:
+    print("C")
+else:
+    print("F")
+
+
+
+
